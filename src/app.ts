0af14adffe4656337af66a9fb49cbee5701b1146
Dan Crowley <dan@agilerium.comd>
2019-05-09 13:38:06 -0400

    added basic redirector

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app: express.Application = express();
+const newDomainUri: string = "https://www.example.com";
 
-app.get('/', (req: Request, res: Response) => {
-	console.log('GET / HTTP VERSION: ', req.httpVersion);
-	res.send('Hello, World');
+app.get("*", (req: Request, res: Response) => {
+  console.log("GET / HTTP VERSION: ", req.originalUrl);
+  const redirectUrl = `${newDomainUri}${req.originalUrl}`;
+  try {
+    res.redirect(301, redirectUrl);
+  } catch (e) {
+    res.status(401).send({ error: "an error occurred" });
+  }
 });
 
 // Express configuration
-app.set('port', process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 
 export default app;
